51dfaee8e14ffd670875a2b4e16dfe4d6d1b4147
Vlad Calomfirescu <vlad.calomfirescu@gmail.com>
2025-12-18 14:46:20 +0200

    created Server struct and cleaned code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,30 +3,12 @@ package main
 import (
 	"chat-room/internal/server"
 	"log/slog"
-	"net"
-	"os"
 )
 
 const addr string = "127.0.0.1:9000"
 
 func main() {
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		slog.Error("Listen returned error", "err", err)
-		os.Exit(1)
-	}
-
-	defer listener.Close()
-
-	slog.Info("Listening", "addr", addr)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			slog.Error("Accept error", "err", err)
-			continue
-		}
-		slog.Info("Client connected", "addr", conn.RemoteAddr())
-		go server.HandleClient(conn)
-	}
+	logger := slog.Default()
+	srv := server.New(addr, logger)
+	srv.Run()
 }
