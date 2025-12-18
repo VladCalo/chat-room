37bc3900a89c8ef93e47e5e1ddaf470a158685eb
Vlad Calomfirescu <vlad.calomfirescu@gmail.com>
2025-12-18 14:13:12 +0200

    added multiple client connections

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"strings"
 	"net"
 	"os"
 )
 
 const addr string = "127.0.0.1:9000"
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	//reading loop for current conn
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("[INFO] Client disconnected:", conn.RemoteAddr())
+				return
+			}
+			fmt.Println("[ERROR] Read had error:", err)
+			return
+		}
+		line = strings.TrimSpace(line)
+		fmt.Printf("[INFO] From %v: %q\n", conn.RemoteAddr(), line)
+
+		data := []byte(line)
+		totalWritten := 0
+		
+		// write data back to client
+		for totalWritten < len(data) {
+			n, werr := conn.Write(data[totalWritten:])
+			if werr != nil {
+				fmt.Println("[ERROR] Writing error to", conn.RemoteAddr(), ":", werr)
+				return
+			}
+			totalWritten += n
+		}
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", addr)
@@ -21,42 +57,14 @@ func main() {
 	fmt.Println("[INFO] Listening on", addr)
 	fmt.Println("[INFO] Waiting for one client to connect...")
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("[EROR] Error accepting connections:", err)
-		os.Exit(1)
-	}
-
-	defer conn.Close()
-	
-	fmt.Println("[INFO] Client connected from", conn.RemoteAddr())
-	fmt.Println("[INFO] Listen loop started...")
-
-	buf := make([]byte, 4)
-
+	// accepting connection from clients loop
 	for {
-		n, err := conn.Read(buf)
+		conn, err := listener.Accept()
 		if err != nil {
-			if err == io.EOF {
-				fmt.Println("client disconnected")
-				return
-			}
-			fmt.Println("[ERROR] Read error:", err)
-			os.Exit(1)
+			fmt.Println("[ERROR] Accept error:", err)
+			continue
 		}
-
-		received := buf[:n]
-		fmt.Printf("Received %d bytes: %q\n", n, string(received))
-
-		totalWritten := 0
-		for totalWritten < n {
-			w, werr := conn.Write(received[totalWritten:])
-			if werr != nil {
-				fmt.Println("[ERROR] Write error:", werr)
-				return
-			}
-			totalWritten += w
-		}
+		fmt.Println("[INFO] Client connected from:", conn.RemoteAddr())
+		go handleClient(conn)
 	}
-
 }
