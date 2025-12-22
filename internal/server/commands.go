b1c3e92ead686a7fd587597c8a59deb9e475242a
Vlad Calomfirescu <vlad.calomfirescu@gmail.com>
2025-12-22 16:46:44 +0200

    exit room

--- internal/server/commands.go
+++ internal/server/commands.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+const lobbyMsg string = "You are in the lobby...Please join a room to send messages!\n"
+
 func (s *Server) handleCommand(client *Client, input string) {
 	parts := strings.Fields(input)
 
@@ -14,14 +16,23 @@ func (s *Server) handleCommand(client *Client, input string) {
 
 	switch parts[0] {
 	case "/join":
-		if len(parts) < 2 {
+		if len(parts) != 2 {
 			s.sendToClient(client, "Usage: /join <room>\n")
 			return
 		}
 		s.joinRoom(parts[1], client)
 	case "/exit":
+		if len(parts) != 1 {
+			s.sendToClient(client, "Usage: /exit\n")
+			return
+		}
+		s.exitRoom(client)
 	case "/list":
 	case "/whereAmI":
+		if len(parts) != 1 {
+			s.sendToClient(client, "Usage: /whereAmI\n")
+			return
+		}
 		s.whereAmI(client)
 	case "/help":
 		s.showHelp(client)
@@ -39,6 +50,7 @@ func (s *Server) showHelp(client *Client) {
 #    /list         - List all rooms                   #
 #    /whereAmI     - Tells you which room you are in  #
 #    /help         - Show this help                   #
+#    Control-C     - Disconnect                       #
 #######################################################
 `
 	s.sendToClient(client, help)
@@ -72,11 +84,25 @@ func (s *Server) joinRoom(roomName string, client *Client) {
 	s.sendToClient(client, fmt.Sprintf("Joined room: %s\n", roomName))
 }
 
-//func (s *Server) exitRoom(client)
+func (s *Server) exitRoom(client *Client) {
+	if client.room == nil {
+		s.sendToClient(client, lobbyMsg)
+		return
+	}
+
+	room := client.room
+
+	room.mu.Lock()
+	delete(room.members, client.client_id)
+	room.mu.Unlock()
+
+	client.room = nil
+	s.sendToClient(client, fmt.Sprintf("Left room: %s\n", room.name))
+}
 
 func (s *Server) whereAmI(client *Client) {
 	if client.room == nil {
-		s.sendToClient(client, "You are in the lobby...Please join a room to send messages!\n")
+		s.sendToClient(client, lobbyMsg)
 		return
 	}
 	s.sendToClient(client, fmt.Sprintf("You are in room: %s\n", client.room.name))
